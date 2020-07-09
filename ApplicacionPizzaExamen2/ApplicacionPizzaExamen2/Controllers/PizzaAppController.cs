00eea73290d687c4c7420fd3116b5bf8ae6bf42b
Felipe <felipesanchezbaldi@gmail.com>
2020-07-08 19:51:49 -0600

    Añadidos los precios a los ingredientes y mejorados los textos de la pagina de incio

--- ApplicacionPizzaExamen2/ApplicacionPizzaExamen2/Controllers/PizzaAppController.cs
+++ ApplicacionPizzaExamen2/ApplicacionPizzaExamen2/Controllers/PizzaAppController.cs
@@ -27,19 +27,13 @@ namespace ApplicacionPizzaExamen2.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
         [HttpPost]
         public ActionResult SumarACarrito(PizzaOrdenModel model) {
             PizzaFacturaModel factura;
